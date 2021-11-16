fdd2b9492ec117b3876739044f0985ec26a32c8b
root <allc>
2021-11-16 14:38:08 +0000

    auth & telescope

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/hello-word', [App\Http\Controllers\MainController::class, 'helloWord']);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
